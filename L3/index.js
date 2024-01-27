86404a564476c65f51b7c95f4743d3a5ed932dab
Sovan Roy <roysovan00@gmail.com>
2024-01-27 13:01:02 +0530

    added lecture 3

---
+++ L3/index.js
@@ -0,0 +1,6 @@
+console.log("Hello there ! I am js");
+
+
+//Note : dom manupulation related stuff is not included in node js
+// console.log(window);  // window is not defined
+// console.log(alert("Hey"));
