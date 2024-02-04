917aebc78f476861bfd4bc153a7e7fdb7bb182c5
Sovan Roy <roysovan00@gmail.com>
2024-02-04 18:42:52 +0530

    added Lecture16 and 17

--- L16,L17/index.js
+++ L16,L17/index.js
@@ -9,7 +9,7 @@ app.get("/", (req, res) => {
 app.get("/user", (req, res) => {
   // res.setHeader("myName", "Sovan Roy"); // custom headers
   res.setHeader("X-MyName", "Sovan Roy"); // custom headers // good practice
-  res.send("Hello World!");
+  res.status(201).send("Hello World!"); 
 });
 
 app.listen(port, () => {
