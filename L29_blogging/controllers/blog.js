7406d5e637c4cef5a52672eadca61aa4e75cb83a
Sovan Roy <roysovan00@gmail.com>
2024-04-09 08:48:41 +0530

    added some changes

--- L29_blogging/controllers/blog.js
+++ L29_blogging/controllers/blog.js
@@ -11,7 +11,7 @@ async function handlePostAddBlog(req, res) {
   // console.log(req.body);
   // console.log(req.file);
   const blog = await Blog.create({
-    // coverImageUrl: `/uploads/${req.file.filename}`,
+    coverImageUrl: `/uploads/${req.file.filename}`,
     title: req.body.title,
     body: req.body.body,
     createdBy: req.user._id,
